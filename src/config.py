3f2170659e1be440f39e26c1abc899652ddbdd37
oystkva <oystkva@stud.ntnu.no>
2026-03-19 16:55:43 +0100

    add: .gitignore commited + project root refactored

--- src/config.py
+++ src/config.py
@@ -1,16 +1,20 @@
 import sys, os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from project_root import PROJECT_ROOT
 
 ######################## CONFIG PARAMETERS #########################
 
 N_CPUs = 1
 TR = 0.8
 
-###################### CONFIG PARAMETERS IDUN SET UP #########################
+###################### CONFIG PATHS #########################
 
-PROJECT_ROOT = "/cluster/home/oystkva/project/code"
 
-FMRI_DIR = "/cluster/projects/itea_lille-ie/Transdiagnostic/output/fmriprep-25.1.4"
+FMRI_DIR = "/cluster/projects/itea_lille-ie/Transdiagnostic/output/fmriprep-25.1.4" # Only on IDUN
 DATA_DIR = os.path.join(PROJECT_ROOT, "data")
 BOLD_DIR = os.path.join(DATA_DIR, "BOLD_signals_parcelled")
 PHENO_PATH = os.path.join(DATA_DIR, "phenotype", "demos.csv")
@@ -19,16 +23,3 @@ HC_LIST = os.path.join(SUBJ_LST_DIR, "hc_subjects.txt")
 MDD_LIST = os.path.join(SUBJ_LST_DIR, "mdd_subjects.txt")
 PLOT_DIR = os.path.join(PROJECT_ROOT, "plots")
 LOG_DIR = os.path.join(PROJECT_ROOT, "logs")
-
-#####################################################################################
-
-######################## CONFIG PARAMETERS LOCAL SET UP #########################
-
-# PROJECT_ROOT = "C:\\Users\\oykva\\OneDrive - NTNU\\Semester 9\\Prosjektoppgave\\project_code"
-
-# FMRI_DIR = os.path.join(PROJECT_ROOT, "fmriprep-25.1.4")
-# DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-# PHENO_PATH = os.path.join(DATA_DIR, "phenotype", "demos.csv")
-# SUBJ_LST_DIR = os.path.join(DATA_DIR, "subject_lists")
-# HC_LIST = os.path.join(SUBJ_LST_DIR, "hc_subjects.txt")
-# MDD_LIST = os.path.join(SUBJ_LST_DIR, "mdd_subjects.txt")
